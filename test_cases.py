8acffca9624a945bf705be48d14a39d763fecb60
Sean Lewis <splewis@utexas.edu>
2014-08-22 11:03:21 -0500

    Update test case.

--- test_cases.py
+++ test_cases.py
@@ -4,7 +4,7 @@ import smbuilder
 
 class SMBuilderTests(unittest.TestCase):
     def test_overall(self):
-        smbuilder.perform_builds('test', 'spcomp')
+        smbuilder.perform_builds('test:all', 'spcomp')
 
 
 if __name__ == "__main__":
