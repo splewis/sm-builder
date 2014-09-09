23f09d02d7114c8fbef1b15f51109bcd862a1862
Sean Lewis <splewis@utexas.edu>
2014-09-08 19:30:54 -0500

    Fix broken test.

--- test/test_cases.py
+++ test/test_cases.py
@@ -11,7 +11,7 @@ class BuilderTests(unittest.TestCase):
     def test_find_plugin_deps(self):
         def fake_package(name, plugins):
             # TODO: this is downright idiotic
-            return smbuilder.base.PackageContainer(name, plugins, {}, [], [], [], [], [], [], '', [], {})
+            return smbuilder.base.PackageContainer(name, plugins, {}, [], [], [], [], [], [], '', [], {}, True)
 
         # TODO: this should really test package inheritance too
         p1 = fake_package('p1', ['plugin1', 'plugin2', 'plugin3'])
