d87f87663842ef743ac430a560e21fec0b3a6537
Sean Lewis <splewis@utexas.edu>
2014-10-31 18:25:24 -0500

    Fix install scripts.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Sean Lewis',
     author_email='splewis@utexas.edu',
     url='https://github.com/splewis/sm-builder',
-    scripts=['scripts/smbuilder', 'scripts/smuploader', 'scripts/smdownload'],
+    scripts=['scripts/smbuilder'],
     packages=['smbuilder'],
     package_dir={'smbuilder': 'src/smbuilder'},
     package_data={'smbuilder': ['plugins/*.sp', 'plugins/smbuild']},
