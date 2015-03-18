fa2b6800cced31cc04a75b3db405462966266738
Sean Lewis <splewis@utexas.edu>
2015-03-18 12:15:17 -0500

    Add smversion script.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Sean Lewis',
     author_email='splewis@utexas.edu',
     url='https://github.com/splewis/sm-builder',
-    scripts=['scripts/smbuilder', 'scripts/smstruct'],
+    scripts=['scripts/smbuilder', 'scripts/smstruct', 'scripts/smversion'],
     packages=['smbuilder'],
     package_dir={'smbuilder': 'src/smbuilder'},
     package_data={'smbuilder': ['plugins/*.sp', 'plugins/smbuild']},
